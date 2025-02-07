1fa9f0471b51d4fff97f6d3a355063d5ea0a8539
root <root@LAPTOP-HJ5VLEH8>
2025-02-07 22:11:07 +0000

    Ingestion changes

--- ingester.py
+++ ingester.py
@@ -7,6 +7,8 @@ from urllib.parse import urljoin
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
+import chromadb
+import time
 
 def get_sub_urls(base_url):
     """Fetch the sub URLs from the base URL."""
@@ -21,20 +23,60 @@ def get_sub_urls(base_url):
     urls = [urljoin(base_url, link.get('href')) for link in soup.find_all('a')]
     return urls
 
-def ingest_website(local_embeddings):
+def url_exists(url, vector_store):
+    # Use the 'where' filter to search for the filename in metadata
+    results = vector_store._collection.get(
+        where={"url": url},
+        include=["metadatas"]
+    )
+    # If results are not empty, the document exists
+    return len(results["metadatas"]) > 0
+
+def ingest_website(local_embeddings, vector_store):
     BASE_URL = "https://www.automatebusiness.com/"
-    # urls = get_sub_urls(BASE_URL)
-    # urls.append(BASE_URL)
-    urls = [BASE_URL]
+    urls = get_sub_urls(BASE_URL)
+    urls.append(BASE_URL)
+    # urls = [BASE_URL]
+
+    # Initialize a list to hold Document objects
+    documents = []
+
+    start_time = time.time()
     for url in urls:
-        loader = WebBaseLoader(url)
-        data = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-        all_splits = text_splitter.split_documents(data)
-        print(len(all_splits))
-        vectorstore = Chroma.from_documents(documents=all_splits, embedding=local_embeddings, persist_directory="vectordb")
+        if not url_exists(url, vector_store):
+            print('Ingesting url:',url)
+            try:
+                loader = WebBaseLoader(url)
+                data = loader.load()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to load {url}: {e}")
+                continue  # Skip to the next URL
+
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+            all_splits = text_splitter.split_documents(data)
+
+            # Add filename as metadata to each chunk
+            for doc in all_splits:
+                doc.metadata["url"] = url
+                documents.append(doc)
+
+            # print(len(all_splits))
+            vector_store.add_documents(documents=documents) 
+            ingested_time = time.time() - start_time
+            print('Ingested url:',url, 'in time',ingested_time)
+            start_time = ingested_time
+        # vectorstore = Chroma.from_documents(documents=all_splits, embedding=local_embeddings, persist_directory="vectordb")
         
 if __name__=="__main__":
     OLLAMA_BASE_URL = "http://100.68.93.4:11434"
+    CHROMA_HOST = "http://100.68.93.4:8100"  
+    CHROMA_PORT = 8100
+    chroma_client = chromadb.HttpClient(host=CHROMA_HOST, port=CHROMA_PORT)
     local_embeddings = OllamaEmbeddings(model="nomic-embed-text", base_url=OLLAMA_BASE_URL)
-    ingest_website(local_embeddings)
+    vector_store = Chroma(
+            client=chroma_client,
+            collection_name="AYB",
+            embedding_function=local_embeddings,
+            persist_directory="AYB_vectordb",  # Where to save data locally, remove if not necessary
+        )
+    ingest_website(local_embeddings, vector_store)
--- rag.py
+++ rag.py
@@ -6,7 +6,11 @@ from langchain_ollama import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+import chromadb
+import ingester
+import time
 
+'''
 # Loading the datas
 # loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
 loader = WebBaseLoader("https://www.automatebusiness.com/")
@@ -15,54 +19,87 @@ data = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
 
-OLLAMA_BASE_URL = "http://100.68.93.4:11434"
-
 # Using embedding model
 local_embeddings = OllamaEmbeddings(model="nomic-embed-text", base_url=OLLAMA_BASE_URL)
 vectorstore = Chroma.from_documents(documents=all_splits, embedding=local_embeddings)
+'''
 
-# question = "What are the approaches to Task Decomposition?"
-# docs = vectorstore.similarity_search(question)
-# print(len(docs))
-# print(docs[0])
+OLLAMA_BASE_URL = "http://100.68.93.4:11434"
 
-# # Using main LLM
-model = ChatOllama(model="llama3.1:8b", base_url=OLLAMA_BASE_URL)
-# response_message = model.invoke(
-#     "Simulate a rap battle between Stephen Colbert and John Oliver"
-# )
+def get_retriever():
+    # Using ingester and server db
+    CHROMA_HOST = "http://100.68.93.4:8100"  
+    CHROMA_PORT = 8100
+    chroma_client = chromadb.HttpClient(host=CHROMA_HOST, port=CHROMA_PORT)
+    local_embeddings = OllamaEmbeddings(model="nomic-embed-text", base_url=OLLAMA_BASE_URL)
+    vector_store = Chroma(
+            client=chroma_client,
+            collection_name="AYB",
+            embedding_function=local_embeddings,
+            persist_directory="AYB_vectordb",  
+        )
+    # ingester.ingest_website(local_embeddings, vector_store)
+    retriever = vector_store.as_retriever()
+    return retriever
 
-# print(response_message.content)
+def setup_rag():
+    # Using main LLM
+    model = ChatOllama(model="llama3.1:8b", base_url=OLLAMA_BASE_URL)
 
-# Q&A with retrieval
-RAG_TEMPLATE = """
-You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+    # Q&A with retrieval
+    RAG_TEMPLATE = """
+    You are customer support for company called Automate Your Business. 
+    Use the following pieces of retrieved context to answer the question. 
+    If you don't know the answer, just say that you don't know. 
+    Use three sentences maximum and keep the answer concise.
 
-<context>
-{context}
-</context>
+    <context>
+    {context}
+    </context>
 
-Answer the following question:
+    Answer the following question:
 
-{question}"""
+    {question}"""
 
-rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
+    rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
 
-retriever = vectorstore.as_retriever()
+    # Convert loaded documents into strings by concatenating their content
+    # and ignoring metadata
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
 
-# Convert loaded documents into strings by concatenating their content
-# and ignoring metadata
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+    qa_chain = (
+        {"context": get_retriever() | format_docs, "question": RunnablePassthrough()}
+        | rag_prompt
+        | model
+        | StrOutputParser()
+    )
 
-qa_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | rag_prompt
-    | model
-    | StrOutputParser()
-)
+    return qa_chain
 
-question = "How can I accelerate business growth?"
+def handle_question(question, qa_chain):
+    start_time = time.time()
+    answer = qa_chain.invoke(question)
+    # print(answer)
+    total_time = time.time()  - start_time
+    print('Total Time:',round(total_time,2))
+    return answer
 
-answer = qa_chain.invoke(question)
-print(answer)
+def main():
+    qa_chain = setup_rag()
+    print("I am Automate Your Business Customer Support. Please ask your queries. Press Ctrl+D followed by Enter to exit.")
+    while True:
+        try:
+            user_input = input("Question: ")
+            if user_input.strip():  # Check if the input is not empty
+                print('Please wait, processing message...')
+                answer = handle_question(user_input, qa_chain)
+                print(f"Answer: {answer}")
+            else:
+                print("Please enter a valid question.")
+        except EOFError:
+            print("\nEnd of input detected. Exiting the program.")
+            break
+
+if __name__ == "__main__":
+    main()
